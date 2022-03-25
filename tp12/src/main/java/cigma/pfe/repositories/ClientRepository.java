c206f827a52fd53d715a3d4fd1d8e43e52659bef
fahimfatima <fatimafahim.dev@gmail.com>
2022-03-25 15:59:36 +0000

    tp13

---
+++ tp12/src/main/java/cigma/pfe/repositories/ClientRepository.java
@@ -0,0 +1,15 @@
+package cigma.pfe.repositories;
+
+
+import cigma.pfe.models.Client;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface ClientRepository extends CrudRepository<Client,Long> {
+    List<Client> findByName(String clt);
+
+    //  List <Client> findByName(String name);
+
+}
